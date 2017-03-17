53f3e2e725454e10fc8dd0344263c77ed1b8dc03
paul van bladel <paul.vanbladel@gmail.com>
2017-03-17 11:42:49 +0100

    debugging in vscode

---
+++ tests/patient-api.spec.ts
@@ -0,0 +1,19 @@
+import { assert, expect } from 'chai';
+
+let chai = require('chai');
+let chaihttp= require('chai-http')
+chai.use(chaihttp);
+
+describe('Patients get', function () {
+  it('should return all patients', function (done) {
+    let url = "http://localhost:8888/api"
+    
+    chai.request(url).get('/patients').end((err, res)=>{
+      assert.isNull(err);
+      debugger;
+      expect(err).to.be.null;
+      expect(res).to.have.status(200);
+      done();
+    });
+  });
+});
