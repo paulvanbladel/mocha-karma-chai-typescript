ff606079b8cbf0a6d027086dae36b68b04d76d9d
paul van bladel <paul.vanbladel@gmail.com>
2017-03-17 10:33:31 +0100

    base framework for typescript based testing

--- tests/arrayTest.js
+++
@@ -1,8 +0,0 @@
-var chai = require('chai');
-var assert = chai.assert;
-describe('array', () => {
-  it('should start empty', () => {
-    var arr = [];
-    assert.equal(arr.length, 0);
-  })
-})
---
+++ tests/calculator.spec.ts
@@ -0,0 +1,11 @@
+import { assert } from 'chai';
+import {Calculator} from '../calculator';
+
+
+var calculator = new Calculator();
+
+describe('Calculator', function () {
+  it('should calculata 1 +2 and return 2', function () {
+    assert.equal(calculator.sum(1,1), 2)
+  });
+});
